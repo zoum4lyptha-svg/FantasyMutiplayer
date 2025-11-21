73fb793a40e54be6691934f2dc63049a395ebe5b
Grey <zoum4lyptha@gmail.com>
2025-11-21 18:03:32 +0800

    feat:实现走，跑，跳等基础的LocoMotion

---
+++ Source/Grey/Private/Animations/GAnimInstance.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animations/GAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UGAnimInstance::NativeInitializeAnimation()
+{
+	// player 或 AI 角色
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	if (OwnerCharacter)
+	{
+		OwnerMovementComp = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UGAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	if (OwnerCharacter)
+	{
+		Speed = OwnerCharacter->GetVelocity().Length();
+
+
+		//简单的求差值，计算yaw角速度，代表旋转有多快
+		// 注意这个是瞬时的，可能会比较快，需要平滑一下
+		FRotator BodyRot = OwnerCharacter->GetActorRotation();
+		FRotator BodyRotDelta = UKismetMathLibrary::NormalizedDeltaRotator(BodyRot, BodyPrevRot);
+		BodyPrevRot = BodyRot;
+
+		YawSpeed = BodyRotDelta.Yaw / DeltaSeconds;
+		SmoothedYawSpeed = UKismetMathLibrary::FInterpTo(SmoothedYawSpeed, YawSpeed, DeltaSeconds, YawSpeedSmoothLerpSpeed);
+	}
+
+	if (OwnerMovementComp)
+	{
+		bIsJumping = OwnerMovementComp->IsFalling();
+	}
+
+}
+
+void UGAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+
+}
---
+++ Source/Grey/Private/Animations/GAnimInstance.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UGAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:	
+
+	// beginplay
+	virtual void NativeInitializeAnimation() override;
+	
+	// tick
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	
+	// safe tick
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+
+	// 为了网络同步的方便，送给动画状态机的变量（如果不是直接从 movementcomponent 拿），尽量在C++写
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSpeed() const { return Speed; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsMoving() const { return Speed != 0; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsNotMoving() const { return Speed == 0; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetYawSpeed() const { return YawSpeed; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSmoothedYawSpeed() const { return SmoothedYawSpeed; }
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool GetIsJumping() const { return bIsJumping; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool GetIsOnGround() const { return !bIsJumping; }
+
+
+private:
+	UPROPERTY()
+	class ACharacter* OwnerCharacter;
+
+	UPROPERTY()
+	class UCharacterMovementComponent* OwnerMovementComp;
+
+
+	float Speed;
+
+	float SmoothedYawSpeed;
+
+	float YawSpeed;
+
+	bool bIsJumping;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	float YawSpeedSmoothLerpSpeed = 1.f;
+
+	FRotator BodyPrevRot;
+};
