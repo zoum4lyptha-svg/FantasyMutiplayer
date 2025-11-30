7ad62bebd57c598ed7a215a1e3d6b0fce8058e7a
Grey <zoum4lyptha@gmail.com>
2025-11-30 09:52:25 +0800

    feat:导入部分UI资产
    主要是能力table和一些图标icon

---
+++ Source/Grey/Private/Widgets/AbilityGauge.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityGauge.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GREY_API UAbilityGauge : public UUserWidget
+{
+	GENERATED_BODY()
+private:
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Icon;
+
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* CooldownCounterText;
+
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* CooldownDurationText;
+
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* CostText;
+};
